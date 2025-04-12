65c01ecca0708222567de310df46c4d389145cdf
João Roldão <jroldao04@ua.pt>
2025-04-12 11:00:24 +0100

    login and register endpoints created

--- app/routes/user_router.py
+++ app/routes/user_router.py
@@ -1,11 +1,19 @@
 from fastapi import APIRouter
-from app.services.supabase_client import select_all_users
-router = APIRouter(
-    prefix="/user",
-    tags=["user"],
-)
 
+from app.models.user import User
+
+from app.services.supabase_client import select_all_users, login_user, register_user
+
+router = APIRouter(prefix="/user", tags=["user"])
 
 @router.get("/")
 async def get_all_users():
     return select_all_users()
+
+@router.post("/login")
+async def login(user: User):
+    return login_user(user)
+
+@router.post("/register")
+async def register(user: User):
+    return register_user(user)
